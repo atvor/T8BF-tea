6ce2804c70da0b656fa004dc89c11a5a942c5864
atvor <atvor@icloud.com>
2024-04-19 20:44:31 +0300

    --fix image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://tea.T8BF.com",
     image: {
-        service: passthroughImageService(),
+        service: squooshImageService(),
     },
     integrations: [tailwind(), mdx(), sitemap()],
 });
