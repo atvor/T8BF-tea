d1be4a88e304132d5b0cbf730419107040c544c5
atvor <atvor@icloud.com>
2024-04-19 19:37:09 +0300

    init

---
+++ src/content/config.ts
@@ -0,0 +1,35 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+    schema: z.object({
+        draft: z.boolean(),
+        title: z.string(),
+        snippet: z.string(),
+        image: z.object({
+            src: z.string(),
+            alt: z.string(),
+        }),
+        publishDate: z.string().transform((str) => new Date(str)),
+        author: z.string().default("Anon"),
+        category: z.string(),
+        tags: z.array(z.string()),
+    }),
+});
+
+const teamCollection = defineCollection({
+    schema: z.object({
+        draft: z.boolean(),
+        name: z.string(),
+        title: z.string(),
+        avatar: z.object({
+            src: z.string(),
+            alt: z.string(),
+        }),
+        publishDate: z.string().transform((str) => new Date(str)),
+    }),
+});
+
+export const collections = {
+    blog: blogCollection,
+    team: teamCollection,
+};
